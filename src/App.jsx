4b286c197091e87f2d6e6f990b1aaafb44a4a2f1
mouram chafikE <mrmouramito@gmail.com>
2023-10-09 19:28:52 +0200

    oh mon dieu jai la tete qui tourne hhhh

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import PokemonCard from './components/PokemonCard';
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
+  const selectedPokemon = pokemonList[0]; // Sélectionnez le Pokémon que vous souhaitez transmettre
   return (
-    <div>
-      <PokemonCard />
+    <div className="App">
+      <PokemonCard pokemon={selectedPokemon} />
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,37 +1,19 @@
 import React from 'react';
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
+function PokemonCard(props) {
+  const { pokemon } = props; // Utilisation de la destructuration pour accéder à props.pokemon
+  console.log(pokemon); // Affiche l'objet pokemon dans la console
 
-
-function PokemonCard() {
-    const pokemon = pokemonList[0];
-    return (
-        <div className="pokemon-card">
-          <h2>{pokemon.name}</h2>
-          {pokemon.imgSrc ? (
-            <img src={pokemon.imgSrc} alt={pokemon.name} />
-          ) : (
-            <p>???</p>
-          )}
-        </div>
-      );
   return (
-    <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-      <figcaption>Bulbasaur</figcaption>
-    </figure>
+    <div className="pokemon-card">
+      <h2>{pokemon.name}</h2>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+    </div>
   );
-
 }
 
 export default PokemonCard;
-
