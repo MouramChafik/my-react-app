63b0e02727db09c86ca03a5b3fc8d99e4fe999e4
mouram chafikE <mrmouramito@gmail.com>
2023-10-09 20:52:54 +0200

    les Pokemon sont presents

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PokemonCard from './components/PokemonCard';
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
-function App() {
-  const selectedPokemon = pokemonList[0]; // Sélectionnez le Pokémon que vous souhaitez transmettre
-  return (
-    <div className="App">
-      <PokemonCard pokemon={selectedPokemon} />
-    </div>
-  );
-}
+  function App() {
+    const [pokemonIndex, setPokemonIndex] = useState(0);
+  
+    const handleNextClick = () => {
+      if (pokemonIndex < pokemonList.length - 1) {
+        setPokemonIndex(pokemonIndex + 1);
+      }
+    };
+  
+    const handlePrevClick = () => {
+      if (pokemonIndex > 0) {
+        setPokemonIndex(pokemonIndex - 1);
+      }
+    };
+  
+    return (
+      <div className="App">
+        <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+        <div className="button-container">
+          {pokemonIndex > 0 && <button onClick={handlePrevClick}>Précédent</button>}
+          {pokemonIndex < pokemonList.length - 1 && <button onClick={handleNextClick}>Suivant</button>}
+        </div>
+      </div>
+    );
+  }
+  
 
 export default App;
